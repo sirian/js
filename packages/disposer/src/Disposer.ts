1e43081b0255a9d817cef60f1d58fc2cd0ed2293
Vasily Khayrulin <sirianru@gmail.com>
2019-06-26 23:03:25 +0300

    patch: inline source map

--- packages/disposer/src/Disposer.ts
+++ packages/disposer/src/Disposer.ts
@@ -1,5 +1,8 @@
 export type DisposeCallback = () => void;
 
+declare function setTimeout(callback: (...args: any[]) => void, ms: number, ...args: any[]): any;
+declare function clearTimeout(timeoutId: any): void;
+
 // Ordered
 enum DisposeState {
     INITIAL = 0,
