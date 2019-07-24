eef60b3645a8b3ca9a0d9c103582b124c21438f8
Vasily Khayrulin <sirianru@gmail.com>
2019-07-24 21:40:57 +0300

    patch(shared-store): use globalThis

--- packages/shared-store/src/SharedStore.ts
+++ packages/shared-store/src/SharedStore.ts
@@ -1,19 +1,15 @@
 import {HybridMap} from "@sirian/common";
 
 const symbol: unique symbol = Symbol.for("@sirian/shared-store");
-const target: { [symbol]?: HybridMap<any, any> } = Symbol as any;
 
 export class SharedStore {
     public static get<T>(key: any, init: () => T) {
+        const target = globalThis as { [symbol]?: HybridMap<any, any> };
+
         if (!target[symbol]) {
             target[symbol] = new HybridMap();
         }
-        const map = target[symbol]!;
 
-        if (!map.has(key)) {
-            map.set(key, init());
-        }
-
-        return map.get(key)! as T;
+        return target[symbol]!.ensure(key, init);
     }
 }
