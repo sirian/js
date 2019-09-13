29184783ced94d93cf183199bba205eb223d1618
Vasily Khayrulin <sirianru@gmail.com>
2019-09-13 23:07:47 +0300

    patch: refactoring

--- packages/common/src/Entries.ts
+++ packages/common/src/Entries.ts
@@ -1,34 +1,31 @@
-import {FromEntries, IterableEntries, ObjEntryOf} from "@sirian/ts-extra-types";
+import {Entry, FromEntries, IterableEntries, ObjEntryOf} from "@sirian/ts-extra-types";
 import {Obj} from "./Obj";
 import {Ref} from "./Ref";
 import {Var} from "./Var";
 
-export class Entries<T extends [any, any]> {
+export class Entries<T extends Entry> {
     protected items: T[];
 
     public constructor(entries: Iterable<T | undefined | null> = []) {
         this.items = Array.from(entries).filter((x) => !Var.isNullable(x)) as T[];
     }
 
-    public static from<E extends [any, any]>(value: IterableEntries<E>): Entries<E>;
+    public static from<E extends Entry>(value: IterableEntries<E>): Entries<E>;
     public static from(value: string): Entries<[number, string]>;
     public static from<T extends object>(value: T): Entries<ObjEntryOf<T>>;
     public static from(value: any) {
         if (Var.isString(value)) {
             value = [...value];
         }
-        if (Ref.hasMethod(value, "entries")) {
-            return new this(value.entries());
-        }
 
-        return this.fromObject(value);
+        const entries = Ref.hasMethod(value, "entries")
+                        ? value.entries()
+                        : Obj.entries(value);
+
+        return new this(entries);
     }
 
-    public static fromObject<T extends object>(target: T) {
-        return new Entries(Obj.entries(target));
-    }
-
-    public map<R extends [any, any]>(callback: <E extends T>(key: E[0], value: E[1]) => R | undefined) {
+    public map<R extends Entry>(callback: <E extends T>(key: E[0], value: E[1]) => R | undefined) {
         const items = this.items.map(([key, value]) => callback(key, value));
         return new Entries(items);
     }
