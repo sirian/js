e1ce06167b5d5ea48550ac39b4090b2cbf7c4085
Vasily Khayrulin <sirianru@gmail.com>
2019-09-13 23:08:23 +0300

    patch(config): ParameterBag.clear

--- packages/config/src/ParameterBag.ts
+++ packages/config/src/ParameterBag.ts
@@ -1,34 +1,81 @@
 import {Entries, Obj, Ref, Var, XSet} from "@sirian/common";
-import {ParameterCircularReferenceError, ParameterNotFoundError} from "./Error";
+import {ParameterBagError, ParameterCircularReferenceError, ParameterNotFoundError} from "./Error";
 
-export class ParameterBag<T extends Record<any, any>> {
+export class ParameterBag<T extends object> {
     protected resolving = new XSet();
-    protected parameters: Partial<T> = {};
+    protected parameters!: T;
     protected resolved = false;
 
     constructor(params?: T) {
-        if (params) {
-            this.add(params);
+        this.parameters = {} as any;
+        this.add(params);
+    }
+
+    public static escapeValue<T>(value: T): T;
+    public static escapeValue(value: any): any {
+        if (Var.isString(value)) {
+            return value.replace(/%/g, "%%");
         }
+
+        if (Var.isPlainArray(value)) {
+            return value.map(ParameterBag.escapeValue);
+        }
+
+        if (Var.isPlainObject(value)) {
+            Entries
+                .from(value)
+                .map((k, v) => [k, this.escapeValue(v)])
+                .toObject();
+        }
+
+        return value;
+    }
+
+    public static unescapeValue<T>(value: T): T;
+    public static unescapeValue(value: any) {
+        if (Var.isString(value)) {
+            return value.replace(/%%/g, "%");
+        }
+        if (Var.isPlainArray(value)) {
+            return value.map(ParameterBag.unescapeValue);
+        }
+        if (Var.isPlainObject(value)) {
+            return Entries
+                .from(value)
+                .map((k, v) => [k, this.unescapeValue(v)])
+                .toObject();
+        }
+
+        return value;
     }
 
     public resolve() {
         if (this.resolved) {
-            return;
+            return this;
         }
 
-        for (const [key, value] of Obj.entries(this.parameters)) {
-            this.parameters[key] = this.unescapeValue(this.resolveValue(value));
+        try {
+            const parameters: Partial<T> = {};
+            for (const [key, value] of Obj.entries(this.parameters)) {
+                const resolved = this.resolveValue(value as T[keyof T]);
+                parameters[key as keyof T] = ParameterBag.unescapeValue(resolved);
+            }
+            this.parameters = parameters as T;
+            this.resolved = true;
+        } finally {
+            this.resolving.clear();
         }
-
-        this.resolved = true;
-        this.resolving.clear();
         return this;
     }
 
-    public add<U extends Record<any, any>>(parameters: U): ParameterBag<T & U> {
+    public clear() {
+        this.parameters = {} as any;
+        return this;
+    }
+
+    public add<U extends object>(parameters?: U): ParameterBag<T & U> {
         Obj.assign(this.parameters, parameters);
-        return this as ParameterBag<T & U>;
+        return this as unknown as ParameterBag<T & U>;
     }
 
     public has(key: PropertyKey) {
@@ -65,7 +112,7 @@ export class ParameterBag<T extends Record<any, any>> {
             let resolved: any = this.get(key);
 
             if (!Var.isString(resolved) && !Var.isNumeric(resolved)) {
-                throw new Error("A string value must be composed of strings/numbers, "
+                throw new ParameterBagError("A string value must be composed of strings/numbers, "
                     + `but found parameter "${key}" of type ${typeof resolved} inside string value "${value}".`);
             }
             resolved = Var.stringify(resolved);
@@ -78,55 +125,20 @@ export class ParameterBag<T extends Record<any, any>> {
         });
     }
 
-    public resolveValue(value: any): any {
+    public resolveValue<V>(value: V): V;
+    public resolveValue(value: any) {
         if (Var.isString(value) && value.length > 2) {
             return this.resolveString(value);
         }
 
-        if (Var.isArray(value)) {
+        if (Var.isPlainArray(value)) {
             return value.map((v) => this.resolveValue(v));
         }
 
-        if (Var.isObject(value)) {
+        if (Var.isPlainObject(value)) {
             return Entries
                 .from(value)
                 .map((k, v) => [this.resolveValue(k), this.resolveValue(v)])
-                .toObject();
-        }
-
-        return value;
-    }
-
-    public escapeValue(value: any): any {
-        if (Var.isString(value)) {
-            return value.replace(/%/g, "%%");
-        }
-
-        if (Var.isArray(value)) {
-            return value.map((v) => this.escapeValue(v));
-        }
-
-        if (Var.isObject(value)) {
-            Entries
-                .from(value)
-                .map((k, v) => [k, this.escapeValue(v)])
-                .toObject();
-        }
-
-        return value;
-    }
-
-    public unescapeValue(value: any): any {
-        if (Var.isString(value)) {
-            return value.replace(/%%/g, "%");
-        }
-        if (Var.isArray(value)) {
-            return value.map((v) => this.unescapeValue(v));
-        }
-        if (Var.isObject(value)) {
-            return Entries
-                .from(value)
-                .map((k, v) => [k, this.unescapeValue(v)])
                 .toObject();
         }
 
