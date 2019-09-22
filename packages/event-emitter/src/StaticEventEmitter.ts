d9491bc10442ebb1c193af122936cb6fa808dfc6
Vasily Khayrulin <sirianru@gmail.com>
2019-09-22 20:51:56 +0300

    patch: StaticEventEmitter

--- packages/event-emitter/src/StaticEventEmitter.ts
+++ packages/event-emitter/src/StaticEventEmitter.ts
@@ -2,62 +2,56 @@ import {EventEmitter, EventEmitterCallback, EventEmitterEventMap} from "./EventE
 import {ListenerOptions} from "./ListenerSet";
 
 interface IEmitterAware<T extends EventEmitterEventMap> {
-    getEmitter(): EventEmitter<T>;
+    emitter: EventEmitter<T>;
 }
 
 export class StaticEventEmitter {
-    protected static emitter?: EventEmitter;
-
-    public static getEmitter() {
-        return this.emitter = this.emitter || new EventEmitter();
-    }
-
     public static on<T extends EventEmitterEventMap, K extends keyof T>(this: IEmitterAware<T>, event: K, listener: EventEmitterCallback<T, K>, opts?: ListenerOptions) {
-        this.getEmitter().on(event, listener, opts);
+        this.emitter.on(event, listener, opts);
         return this;
     }
 
     public static addListener<T extends EventEmitterEventMap, K extends keyof T>(this: IEmitterAware<T>, event: K, listener: EventEmitterCallback<T, K>, opts?: ListenerOptions) {
-        this.getEmitter().addListener(event, listener, opts);
+        this.emitter.addListener(event, listener, opts);
         return this;
     }
 
     public static getListeners<T extends EventEmitterEventMap, K extends keyof T>(this: IEmitterAware<T>, event: K) {
-        return this.getEmitter().getListeners(event);
+        return this.emitter.getListeners(event);
     }
 
     public static emit<T extends EventEmitterEventMap, K extends keyof T>(this: IEmitterAware<T>, event: K, ...args: T[K]) {
-        this.getEmitter().emit(event, ...args);
+        this.emitter.emit(event, ...args);
         return this;
     }
 
     public static once<T extends EventEmitterEventMap, K extends keyof T>(this: IEmitterAware<T>, event: K, listener: EventEmitterCallback<T, K>, opts?: ListenerOptions) {
-        this.getEmitter().once(event, listener, opts);
+        this.emitter.once(event, listener, opts);
         return this;
     }
 
     public static hasListener<T extends EventEmitterEventMap, K extends keyof T>(this: IEmitterAware<T>, event: K, listener: EventEmitterCallback<T, K>) {
-        return this.getEmitter().hasListener(event, listener);
+        return this.emitter.hasListener(event, listener);
     }
 
     public static hasListeners<T extends EventEmitterEventMap>(this: IEmitterAware<T>, event?: keyof T): boolean;
 
     public static hasListeners(this: IEmitterAware<any>, ...args: any[]) {
-        return this.getEmitter().hasListeners(...args);
+        return this.emitter.hasListeners(...args);
     }
 
     public static off<T extends EventEmitterEventMap, K extends keyof T>(this: IEmitterAware<T>, event: K, listener: EventEmitterCallback<T, K>) {
-        this.getEmitter().off(event, listener);
+        this.emitter.off(event, listener);
         return this;
     }
 
     public static removeListener<T extends EventEmitterEventMap, K extends keyof T>(this: IEmitterAware<T>, event: K, listener: EventEmitterCallback<T, K>) {
-        this.getEmitter().removeListener(event, listener);
+        this.emitter.removeListener(event, listener);
         return this;
     }
 
     public static removeAllListeners(this: IEmitterAware<any>) {
-        this.getEmitter().removeAllListeners();
+        this.emitter.removeAllListeners();
         return this;
     }
 }
