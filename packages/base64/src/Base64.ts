ef5b87cc93ff2a1514bc40a4f841df72fd662622
Vasily Khayrulin <sirianru@gmail.com>
2020-02-28 11:41:39 +0300

    patch(base64): update deps

--- packages/base64/src/Base64.ts
+++ packages/base64/src/Base64.ts
@@ -1,10 +1,10 @@
-import {ByteArray, ByteArrayInput} from "@sirian/common";
+import {ByteArray, ByteArraySource} from "@sirian/common";
 import {base64Decode} from "./base64Decode";
 import {base64Encode} from "./base64Encode";
 import {base64Test} from "./const";
 
 export const Base64 = {
-    encode: (value: ByteArrayInput) => ByteArray.from(base64Encode(ByteArray.from(value))),
-    decode: (value: ByteArrayInput) => ByteArray.from(base64Decode(ByteArray.stringify(value))),
+    encode: (value: ByteArraySource) => ByteArray.from(base64Encode(ByteArray.from(value))),
+    decode: (value: ByteArraySource) => ByteArray.from(base64Decode(ByteArray.stringify(value))),
     test: base64Test,
 };
