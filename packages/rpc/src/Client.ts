c1527b1be7df307a720871a9a887fbdc04b4cacd
Vasily Khayrulin <sirianru@gmail.com>
2020-06-17 17:03:30 +0400

    @feat(rpc): timeoutMS

--- packages/rpc/src/Client.ts
+++ packages/rpc/src/Client.ts
@@ -1,10 +1,16 @@
-import {XMap} from "@sirian/common";
+import {isObject, XMap} from "@sirian/common";
 import {EventEmitter} from "@sirian/event-emitter";
 import {XPromise} from "@sirian/xpromise";
 import {AbstractTransport} from "./AbstractTransport";
 import {PayloadType, RequestPayload} from "./Payload";
 import {RPCParams, RPCProtocol, RPCResult} from "./Server";
 import {TransportEvent} from "./TransportEvent";
+
+export interface InvokeInit<T extends RPCProtocol, K extends keyof T> {
+    method: K;
+    params: RPCParams<T, K>;
+    timeoutMS?: number;
+}
 
 export default class Client<T extends RPCProtocol> extends EventEmitter {
     protected requestId: number = 1;
@@ -19,13 +25,25 @@ export default class Client<T extends RPCProtocol> extends EventEmitter {
         transport.onMessage.addListener((e) => this.handleResponse(e));
     }
 
-    public invoke<K extends keyof T>(method: K, ...params: RPCParams<T, K>) {
+    public invoke<K extends keyof T>(init: InvokeInit<T, K>): XPromise<RPCResult<T, K>>;
+    public invoke<K extends keyof T>(method: K, ...params: RPCParams<T, K>): XPromise<RPCResult<T, K>>;
+    public invoke<K extends keyof T>(methodOrInit: K | InvokeInit<T, K>, ...rest: [] | RPCParams<T, K>) {
+        const init = isObject(methodOrInit)
+                     ? methodOrInit
+                     : {method: methodOrInit, params: rest};
+
         const {transport, requests} = this;
+        const {method, params, timeoutMS} = init;
 
         const id = this.requestId++;
 
-        const xPromise = requests.ensure(id, () => new XPromise<RPCResult<T, K>>());
+        const xPromise = new XPromise();
+        requests.set(id, xPromise);
         xPromise.finally(() => requests.delete(id));
+
+        if (timeoutMS) {
+            xPromise.setTimeout(timeoutMS);
+        }
 
         const payload: RequestPayload<T, K> = {
             type: PayloadType.REQUEST,
