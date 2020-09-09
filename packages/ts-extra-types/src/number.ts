31dae19941f2fa99f50afbeec83203461fdd7445
Vasily Khayrulin <sirianru@gmail.com>
2020-09-09 11:37:32 +0300

    feat(ts-extra-types): type Splice<List, Start, DeleteCount, InsertItems>

--- packages/ts-extra-types/src/number.ts
+++ packages/ts-extra-types/src/number.ts
@@ -1,8 +1,22 @@
-// tslint:disable:max-line-length
+export type Numbers = {
+    "0": 0, "1": 1, "2": 2, "3": 3, "4": 4,
+    "5": 5, "6": 6, "7": 7, "8": 8, "9": 9,
+    "10": 10, "11": 11, "12": 12, "13": 13, "14": 14, "15": 15,
+};
 
-export type Numbers = { "0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "11": 11, "12": 12, "13": 13, "14": 14, "15": 15 };
-export type Increments = { 0: 1, 1: 2, 2: 3, 3: 4, 4: 5, 5: 6, 6: 7, 7: 8, 8: 9, 9: 10, 10: 11, 11: 12, 12: 13, 13: 14, 14: 15 };
-export type Decrements = { 0: unknown, 1: 0, 2: 1, 3: 2, 4: 3, 5: 4, 6: 5, 7: 6, 8: 7, 9: 8, 10: 9, 11: 10, 12: 11, 13: 12, 14: 13, 15: 14 };
+export type Increments = {
+    0: 1, 1: 2, 2: 3, 3: 4, 4: 5,
+    5: 6, 6: 7, 7: 8, 8: 9, 9: 10,
+    10: 11, 11: 12, 12: 13, 13: 14, 14: 15,
+};
+export type Decrements = {
+    0: unknown, 1: 0, 2: 1, 3: 2, 4: 3,
+    5: 4, 6: 5, 7: 6, 8: 7, 9: 8,
+    10: 9, 11: 10, 12: 11, 13: 12, 14: 13, 15: 14,
+};
+
+export type Decs = keyof Decrements;
+export type Incs = keyof Increments;
 
 export type Inc<N extends number> = N extends keyof Increments ? Increments[N] : number;
 export type Dec<N extends number> = N extends keyof Decrements ? Decrements[N] : number;
@@ -10,7 +24,8 @@ export type Dec<N extends number> = N extends keyof Decrements ? Decrements[N] :
 export type Add<X extends number, Y extends number> =
     X extends 0 ? Y :
     Y extends 0 ? X :
-    {
-        0: number,
-        1: Add<Inc<X>, Dec<Y>>,
-    }[number extends X | Y ? 0 : 1];
+    X extends keyof Increments
+    ? Y extends keyof Decrements
+      ? Add<Inc<X>, Dec<Y>>
+      : number
+    : number;
--- packages/ts-extra-types/src/tree.ts
+++ packages/ts-extra-types/src/tree.ts
@@ -1,13 +1,11 @@
-import {Cons, Length, Tail} from "./tuple";
+import {Length, Tail} from "./tuple";
 import {IsExact} from "./types";
 
 export type NodePath<T, K = "parent"> =
     K extends keyof T
     ? IsExact<T, T[K]> extends true
       ? T[]
-      : Cons<T, NodePath<T[K]>>
-    : T extends object
-      ? [T]
-      : [];
+      : [T, ...NodePath<T[K], K>]
+    : T extends object ? [T] : [];
 
 export type NodeDepth<T, K = "parent"> = Length<Tail<NodePath<T, K>>>;
