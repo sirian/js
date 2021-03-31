3f7e23d5758ee188785d54d2c8d7a984a91f4010
Vasily Khayrulin <sirianru@gmail.com>
2021-04-01 00:21:05 +0300

    patch(common): fix

--- packages/common/src/Async.ts
+++ packages/common/src/Async.ts
@@ -1,6 +1,8 @@
-import {Func0} from "@sirian/ts-extra-types";
+import {Func, Func0} from "@sirian/ts-extra-types";
 import {isPromiseLike} from "./Is";
 import {applyIfFunction} from "./Ref";
+
+declare function setTimeout(fn: Func, ms: number): void;
 
 export async function tryAsync<T>(fn: () => T | PromiseLike<T>): Promise<T | undefined>;
 export async function tryAsync<T, R>(fn: () => T | PromiseLike<T>, onError: R | PromiseLike<R> | ((err: any) => R | PromiseLike<R>)): Promise<T | R>;
