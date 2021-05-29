85c28c6df1126c471b802908125ed2303d8ef43a
Vasily Khayrulin <sirianru@gmail.com>
2021-05-30 02:22:45 +0300

    fix(event-loop): ES2020

--- packages/event-loop/src/EventLoop.ts
+++ packages/event-loop/src/EventLoop.ts
@@ -3,10 +3,10 @@ import {Interval} from "./Interval";
 import {TaskCallback} from "./TaskQueue";
 import {Timeout} from "./Timeout";
 
+export const sleep = (ms: number) => new Promise<void>((resolve) => setTimeout(resolve, ms));
+
 export const startImmediate = (callback: TaskCallback) => Immediate.start(callback);
 
 export const startTimeout = (ms: number, callback: TaskCallback) => Timeout.start(ms, callback);
 
 export const startInterval = (ms: number, callback: TaskCallback) => Interval.start(ms, callback);
-
-export const sleep = (ms: number) => new Promise<void>((resolve) => setTimeout(resolve, ms));
