49f5534e093dbc51395514020f0c80938afabd05
Vasily Khayrulin <sirianru@gmail.com>
2021-08-12 14:50:20 +0300

    patch(common): use MaybePromise

--- packages/common/src/async.ts
+++ packages/common/src/async.ts
@@ -1,10 +1,10 @@
-import {Func, Func0} from "@sirian/ts-extra-types";
+import {Func, Func0, MaybePromise} from "@sirian/ts-extra-types";
 import {applyIfFunction} from "./ref";
 
 declare function setTimeout(fn: Func, ms: number): void;
 
-export async function tryAsync<T>(fn: () => T | PromiseLike<T>): Promise<T | undefined>;
-export async function tryAsync<T, R>(fn: () => T | PromiseLike<T>, onError: R | PromiseLike<R> | ((err: any) => R | PromiseLike<R>)): Promise<T | R>;
+export async function tryAsync<T>(fn: () => MaybePromise<T>): Promise<T | undefined>;
+export async function tryAsync<T, R>(fn: () => MaybePromise<T>, onError: MaybePromise<R> | ((err: any) => MaybePromise<R>)): Promise<T | R>;
 export async function tryAsync(fn: Func0, onError?: any) {
     try {
         return await fn();
